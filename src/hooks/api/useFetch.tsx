e1dc756ebbbcc3bf66d20008b07128d4762fc148
Pavel Chulanov <pavel.chulanov@lumiktech.com>
2023-06-06 14:00:58 +0300

    Add api converter and group api hooks

--- src/hooks/api/useFetch.tsx
+++ src/hooks/api/useFetch.tsx
@@ -1,30 +1,27 @@
 import {useEffect, useState} from "react";
 import {getErrorMessage} from "../helpers/errorHandler";
+import {IFetch} from '../types/api/fetch';
+import { httpMethod } from "../types/api/http";
 
 export const useFetch = <T,>(
     url: string,
-    options: RequestInit,
-    initialState: T,
-    loadOnStart: boolean = true
-    ): [boolean, T | undefined, string, () => void] => {
+    method: httpMethod,
+    initialState: T
+    ): IFetch<T> => {
 
     const [data, setData] = useState<T>(initialState);
     const [loading, setLoading] = useState<boolean>(true);
     const [errorMessage, setErrorMessage] = useState<string>('')
 
     useEffect(() => {
-        if (loadOnStart) sendRequest()
-    }, [loadOnStart]);
-
-    const request = () => {
-        sendRequest();
-    }
+        sendRequest()
+    }, [])
 
     async function sendRequest() {
         setLoading(true);
 
         try {
-            const response = await fetch(url, options)
+            const response = await fetch(url, {method})
 
             if (!response.ok) {
                 throw new Error(response.statusText)
@@ -40,10 +37,9 @@ export const useFetch = <T,>(
 
     }
 
-    return [
+    return {
         loading,
         data,
-        errorMessage,
-        request
-    ];
+        errorMessage
+    };
 };
